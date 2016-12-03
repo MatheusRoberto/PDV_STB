49aec536cdfb59c1edd14624c8a7931dd9d785a9
matheus <matheus@matheus-Aspire-5750>
2016-12-03 12:28:19 -0200

    Colocado tema BlackMoon

--- src/projetotcc/ProjetoTcc.java
+++ src/projetotcc/ProjetoTcc.java
@@ -6,6 +6,9 @@
 package projetotcc;
 
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 
 /**
  *
@@ -18,6 +21,18 @@ public class ProjetoTcc {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        /**
+         * @autor SID
+         * Adicionando o código de invocação do tema
+         * Invocação direta da tela inicial
+         */
+        try{
+            UIManager.setLookAndFeel("de.javasoft.plaf.synthetica.SyntheticaBlackMoonLookAndFeel");
+        }catch (ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException e){
+            e.printStackTrace();
+         }    
+        
         JFrame janela = new Tela_Inicial1();
         janela.setVisible(true);
         janela.setLocationRelativeTo(null);
